d61652b2f81ce8862ae18e8d6b035fe18fbc8200
Aaron Kenney <Aaron@canadiancoder.ca>
2025-02-25 00:08:32 -0700

    Add index.php and stylesheet_minify.css

---
+++ index.php
@@ -0,0 +1,366 @@
+<?php
+session_start();
+?>
+
+
+<!DOCTYPE html>
+
+<html>
+
+<head>
+  <meta content="text/html; charset=utf-8" />
+  <title>Canadian Coder – Web Developer and Web Designer in Edmonton</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta content="Aaron Kenney is an Edmonton-based freelance website designer and developer building modern, dynamic websites for clients and small businesses." name="description">
+  
+
+  <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@200&display=swap" rel="stylesheet" />
+
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
+  
+ 
+    
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+
+  
+  <style>
+  
+  #container {
+    background-image: url(images-webp/bg_image_comp.webp);
+}
+
+  
+    @media screen and (max-width: 600px) {
+  #container {
+    background-image: none;
+  }
+  #home {
+      background-image: url(images-webp/home_bg_image_mobile.webp) !important;
+    background-attachment: fixed;
+    background-position: top;
+    background-repeat: no-repeat;
+    background-size: cover;
+    height: 120vh;
+    margin-bottom: 0;
+  }
+    
+  </style>
+    
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+<link rel="manifest" href="/site.webmanifest">
+
+  <link rel="stylesheet" href="stylesheet_minify.css">
+
+ <link rel="icon" type="image/x-icon" href="/favicon.ico">
+  
+
+
+</head>
+
+<body>
+
+ <nav class="topNav" id="myNav">
+        <img id="logo" alt="Canadian Coder logo, website design" src="images/canadian_coder_logo.png" />
+      <a href="javascript:void(0);" class="icon" onclick="myFunction()">
+        <i class="fa fa-bars"></i>
+      </a>
+      <ul id="menu">
+        <li><a class="info" href="#home">Home</a></li>
+        <li><a class="info" href="#about">About</a></li>
+         <li><a class="info" href="#testimonials">Testimonials</a></li>
+        <li><a class="info" href="#work">Work</a></li>
+        <li><a class="info" href="#contact">Contact</a></li>
+      </ul>
+    </nav>
+
+<div id="container">
+ 
+
+ 
+<div id="home">
+    <div id="home-inner">
+      <h1>Aaron Kenney</h1> 
+      <h6>Freelance Web Developer</h6>
+    </div>
+     </div>
+
+    
+      <div id="body-wrap">
+
+    <div id="about">
+      <div id="aboutBody">
+        <h2>About</h2>
+       
+        <div id="flexContainer1">
+          <div id="flexAbout1">
+            <p>
+              My name is Aaron Kenney, I am a web developer and designer based in Edmonton, Alberta, Canada. I
+              have completed internationally recognized
+              certifications for five popular computer languages used in web
+              development:
+            </p>
+            <!-- <br>  -->
+
+            <ul id="certs">
+              <li class="cert">
+                <a class="biolink" target="_blank" href="images-webp/html5_certificate.webp">- HTML</a>
+              </li>
+
+              <li class="cert">
+                <a class="biolink" target="_blank" href="images-webp/css3_certificate.webp">- CSS</a>
+              </li>
+
+              <li class="cert">
+                <a class="biolink" target="_blank" href="images-webp/javascript_certificate.webp">- JavaScript</a>
+              </li>
+
+              <li class="cert">
+                <a class="biolink" target="_blank" href="images-webp/php_certificate.webp">- PHP</a>
+              </li>
+
+              <li class="cert">
+                <a class="biolink" target="_blank" href="images-webp/python_certificate.webp">- Python</a>
+              </li>
+
+            </ul>
+            <!-- <br>  -->
+            
+             <p> I also have experience in other web-based technologies and platforms, including MySQL, Bootstrap, Wordpress and Shopify.</p>
+                        <br>
+
+            
+            <p>
+              I am a graduate of the University of Alberta, where I earned a
+              bachelor’s degree in philosophy and english. The attention to detail, critical
+              thinking and problem-solving skills I acquired studying philosophy have been instrumental
+              in furthering my skills as a developer.</p>
+              <br>
+          </div>
+
+          <div id="flexAbout2">
+            <p>I enjoy writing on various topics including technology, and have published science-related articles appearing in <span class="italic">Cognosco</span>, the Edmonton Brain Care Centre’s
+              newsletter:
+            </p>
+            <!-- <br>  -->
+
+            <ul id="certs">
+                 <li class="cert">
+                                    <a class="biolink" target="_blank" href="Small_Business_and_SEO_in_the_Digital_Age.html">- Small Business and SEO in the Digital Age</a>
+                                </li>
+                  <li class="cert">
+                                    <a class="biolink" target="_blank" href="The_Biology_of_a_Website.html">- The Biology of a Website</a>
+                                </li>
+              <li class="cert">
+                <a class="biolink" target="_blank" href="images/Feb_Newsletter.pdf#page=17">- CTE's Deadly Link to Athletes and the Military</a>
+              </li>
+
+              <li class="cert">
+                <a class="biolink" target="_blank" href="images/May_Newsletter.pdf#page=19">- A New Weapon in the Fight Against Mental Illness</a>
+              </li>
+
+              <li class="cert">
+                <a class="biolink">- Potential Breakthrough in Alzheimer's Research</a>
+              </li>
+
+            </ul>
+            <!-- <br>  -->
+
+            <p>
+              I love web development and web design for the nature of the work; the analytical thinking and attention to detail integral to developing a modern, appealing website or web application. I look forward to designing your website and helping you grow your business.
+            </p>
+            
+          </div>
+        
+          <div id="flexAbout3"> <div class="image-wrap"><img class="image" src="images-webp/Aaron_Kenney_photo.webp" alt="headshot of Aaron Kenney, web developer and website design" /></div>
+           <img id="logo-about" title="Canadian Coder" alt="Canadian Coder logo, website design" src="images/canadian_coder_logo.png" />
+          </div>
+        </div>
+      </div>
+    </div>
+    
+    
+     <div id="testimonials">
+            <div id="testimonialsBody">
+                <h2>Testimonials</h2>
+                  <hr class="gradient">
+  <div id="quoteWrap">
+                        <p class="quote">
+                          “1st Class professional that gets the job done!”<br><br>
+                        </p>
+                        <p class="testimonialsLink">-Jeff Musson, <a class="email" target="_new" href="https://www.codingforveterans.com">Coding for Veterans</a>, Toronto, ON</p>
+                </div>
+                  <hr class="gradient">
+                <div id="quoteWrap">
+                        <p class="quote">
+                         “Aaron did a great job making my website! He was very professional and easy to talk to, highly recommended to anyone in the Edmonton or St. Albert area who needs a website for their business.”<br><br>
+                        </p>
+                        <p class="testimonialsLink">-Jeevaraj Sangaralingam, <a class="email" target="_new" href="https://www.riganikitchen.ca">Rigani Greek Fusion Kitchen</a>, St. Albert, AB</p>
+                </div>
+                   <hr class="gradient">
+                <div id="quoteWrap">
+                        <p class="quote">
+                         “Aaron truly cares about the quality of work and is kind, engaging and warm in the manner in which he approached this project. I am really grateful for all your help, Aaron.”<br><br>
+                        </p>
+                        <p class="testimonialsLink">-Hari Shenoy, Dublin, IRL (via <a class="email" target="_new" href="https://www.upwork.com/freelancers/~017f0653325bdcea76">Upwork</a>)</p>
+                </div>
+            </div>
+        </div>
+    
+    
+    
+
+<div id="work">
+            <div id="workBody">
+                <h2>Work</h2>
+                <div id="flexContainer1">
+                     <div class="flex1">
+                        <div class="projectWrap">
+                            <img class="project" alt="Viva La Cena, restaurant website" src="images-webp/vivaLaCenaScreenshot_comp.webp" />
+                            <h5><a class="projectLink" target="_blank" href="https://www.vivalacena.ca">Viva La Cena</a></h5>
+                        </div>
+                    </div> 
+                     <div class="flex2">
+                        <div class="projectWrap">
+                            <img class="project" alt="Apex Fitness Industries website, Edmonton, Alberta" src="images-webp/apexFitnessScreenshot.webp" />
+                            <h5><a class="projectLink" target="_blank" href="https://www.apexfitnessindustries.com/">Apex Fitness Industries</a></h5>
+                        </div>
+                    </div>
+                      </div>
+                       <div id="flexContainerInner">
+                     <div class="flex1">
+                        <div class="projectWrap">
+                            <img class="project" alt="Coding for Veterans website website" src="images-webp/codingForVeteransScreenshot.webp" />
+                            <h5><a class="projectLink" target="_blank" href="https://www.codingforveterans.com">Coding For Veterans</a></h5>
+                        </div>
+                    </div> 
+                     <div class="flex2">
+                        <div class="projectWrap">
+                            <img class="project" alt="Hari Shenoy website" src="images-webp/thoughtBrownieScreenshot_min.webp" />
+                            <h5><a class="projectLink" target="_blank" href="https://www.thoughtbrownie.com/">Hari Shenoy</a></h5>
+                        </div>
+                    </div>
+                      </div> 
+                      <div id="flexContainer2">
+                     <div class="flex1">
+                        <div class="projectWrap">
+                            <img class="project" alt="Rigani Kitchen St. Albert restaurant" src="images-webp/riganiKitchenScreenshot_comp.webp" />
+                            <h5><a class="projectLink" target="_blank" href="https://www.riganikitchen.ca">Rigani Greek Fusion Kitchen</a></h5>
+                        </div>
+                    </div>
+                           <div class="flex2">
+                        <div class="projectWrap">
+                            <img class="project" alt="Cinema Trivia, Edmonton website" src="images-webp/cinemaTriviaScreenshot.webp" />
+                            <h5><a class="projectLink" target="_blank" href="https://www.canadiancoder.ca/cinema_trivia.php">Cinema Trivia</a></h5>
+                        </div>
+                    </div> 
+                </div>
+            </div>
+            
+ </div>
+
+    <div id="contact">
+      <div id="contactBody">
+        <h2>Contact</h2>
+        <p style="margin-top: 30px;" class="contact">I can be contacted at <a class="email" href="mailto: Aaron@CanadianCoder.ca">Aaron@CanadianCoder.ca</a>, or feel free to use the form below:</p>
+      </div>
+
+      <form id="form1" method="post" action="contact_form_process.php" novalidate>
+
+
+        <?php
+
+
+        if (isset($_SESSION["emptyName"])) {
+
+          unset($_SESSION['emptyName']);
+
+          echo "<p class='error'>* Please enter your name</p>";
+        } else if (isset($_SESSION["emptyEmail"])) {
+          unset($_SESSION['emptyEmail']);
+          echo "<p class='error'>* Please enter a valid email address</p>";
+        } else if (isset($_SESSION["emptyMessage"])) {
+          unset($_SESSION['emptyMessage']);
+          echo "<p class='error'>* Please enter a message</p>";
+        } else if (isset($_SESSION["invalidEmail"])) {
+          unset($_SESSION['invalidEmail']);
+          echo "<p class='error'>* Please enter a valid email address</p>";
+        } else if (isset($_SESSION["success"])) {
+
+          unset($_SESSION['success']);
+          echo "<p class='success'>Your message was sent!</p>";
+        }
+
+        ?>
+
+        <div class="textbox">
+
+          <label>Full Name:</label>
+          <input type="text" name="name" placeholder="Name" value="<?php echo isset($_SESSION['name']) ? $_SESSION['name'] : '';
+                                                                    unset($_SESSION['name']); ?>" />
+        </div>
+
+        <div class="textbox">
+
+          <label>Email:</label>
+          <input type="text" name="email" placeholder="Email Address" value="<?php echo isset($_SESSION['email']) ? $_SESSION['email'] : '';
+                                                                              unset($_SESSION['email']) ?>" />
+
+
+        </div>
+
+        <div class="textbox">
+
+          <label>Message:</label>
+          <textarea name="message" placeholder="Message" rows="15" cols="30"><?php echo isset($_SESSION['message']) ? htmlspecialchars($_SESSION['message'], ENT_QUOTES) : '';
+                                                                              unset($_SESSION['message']) ?></textarea>
+
+        </div>
+
+        <div class="button">
+          <button name="submit" type="submit" value="Submit" form="form1">Submit</button>
+        </div>
+
+      </form>
+
+
+    </div>
+    
+    
+     <div id="footer">
+            <div id="flexFooter">
+                <div id="flexFooter1">
+                    <img id="logo-footer" title="Canadian Coder" alt="Canadian Coder logo, website design" src="images/canadian_coder_logo.png" />
+                </div>
+                <!--    <a href="javascript:void(0);" class="icon" onclick="myFunction()">
+                <i class="fa fa-bars"></i>
+                         </a> -->
+                <div id="flexFooter2">
+                    <ul style="margin: 0 auto;" id="certs">
+                        <li class="cert"><a class="footerLinks" href="#home">Home</a></li>
+                        <li class="cert"><a class="footerLinks" href="#about">About</a></li>
+                        <li class="cert"><a class="footerLinks" href="#testimonials">Testimonials</a></li>
+                    </ul>
+                </div>
+                <div id="flexFooter3">
+                    <ul style="margin: 0 auto;" id="certs">
+                        <li class="cert"><a class="footerLinks" href="#work">Work</a></li>
+                        <li class="cert"><a class="footerLinks" href="#contact">Contact</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    
+    
+    
+    </div>
+    </div>
+    
+  
+  <script src="homepage.min.js" defer></script>
+
+</body>
+
+</html>
